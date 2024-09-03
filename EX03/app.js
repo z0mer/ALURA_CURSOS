9d71777b3a31366459285a2555d6c5135bb00a9f
Anna Carolina Zomer <117862465+z0mer@users.noreply.github.com>
2024-09-02 22:20:29 -0300

    Create app.js

---
+++ EX03/app.js
@@ -0,0 +1,23 @@
+let contador = 1;
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
+let contador = 10;
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
+let numeroMaximo = prompt("Digite um número para a contagem regressiva:");
+
+while (numeroMaximo >= 0) {
+    console.log(numeroMaximo);
+    numeroMaximo--;
+}
+let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
+let contador = 0;
+
+while (contador <= numeroMaximo) {
+    console.log(contador);
+    contador++
+}

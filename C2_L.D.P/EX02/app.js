1480583192ac0beb3109504a0b423197f6f8d637
z0mer <annacarolrpz@gmail.com>
2024-09-03 15:18:40 -0300

    desafio 02

---
+++ C2_L.D.P/EX02/app.js
@@ -0,0 +1,39 @@
+function exibirOla() {
+    console.log("Olá, mundo!");
+  }
+  
+  exibirOla();
+
+  function exibirOlaNome(nome) {
+    console.log(`Olá, ${nome}!`);
+  }
+  
+  exibirOlaNome("Anna Carolina");
+
+  function calcularDobro(numero) {
+    return numero * 2;
+  }
+  
+  let resultadoDobro = calcularDobro(5);
+  console.log(resultadoDobro);
+
+  function calcularMedia(a, b, c) {
+    return (a + b + c) / 3;
+  }
+  
+  let media = calcularMedia(4, 7, 10);
+  console.log(media);
+
+  function encontrarMaior(a, b) {
+    return a > b ? a : b;
+  }
+  
+  let maiorNumero = encontrarMaior(15, 9);
+  console.log(maiorNumero);
+
+  function quadrado(numero) {
+    return numero * numero;
+  }
+  
+  let resultado = quadrado(2);
+  console.log(resultado); 

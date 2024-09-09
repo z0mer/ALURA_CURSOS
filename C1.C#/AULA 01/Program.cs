4b64437d0f70df0863b61746a1277f1c90c10857
Anna Carolina Zomer <117862465+z0mer@users.noreply.github.com>
2024-09-09 13:38:19 -0300

    Create Program.cs

---
+++ C1.C#/AULA 01/Program.cs
@@ -0,0 +1,13 @@
+// Screen Sound
+string mensagemDeBoasVindas = "Bem vindo ao Screen Sound";
+Console.WriteLine(mensagemDeBoasVindas);
+
+//1
+string curso = "C# iniciante";
+//2
+Console.WriteLine(curso);
+//3
+string nome = "Anna Carolina";
+string sobrenome = "Zomer";
+//4
+Console.WriteLine("Daniel Portugal e Gui Lima"); 

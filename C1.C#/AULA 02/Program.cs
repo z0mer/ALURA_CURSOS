38877cba41c2f2eecfec38808157a4eb90b5b99b
Anna Carolina Zomer <117862465+z0mer@users.noreply.github.com>
2024-09-10 09:25:56 -0300

    Create Program.cs

---
+++ C1.C#/AULA 02/Program.cs
@@ -0,0 +1,47 @@
+// Screen Sound
+string mensagemDeBoasVindas = "Bem vindo(a) ao Screen Sound";
+
+void ExibirLogo() 
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(" ");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda.");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas.");
+    Console.WriteLine("Digite 3 para avaliar uma banda.");
+    Console.WriteLine("Digite 4 para mostrar a média de uma banda.");
+    Console.WriteLine("Digite 0 para sair.");
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    // muitos recursos de if e else, então é indicado o uso do switch 
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 0: Console.WriteLine("Você saiu do menu");
+            break;
+        default: Console.WriteLine("Opção inválida, digite um número do menu");
+            break;
+    }
+}
+
+ExibirLogo();
+ExibirMenu();

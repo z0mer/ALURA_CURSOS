9f560a6310a0098d2642e524e7ecc58278871116
Anna Carolina Zomer <117862465+z0mer@users.noreply.github.com>
2024-09-11 14:50:18 -0300

    Update Musica.cs

--- !C#/C2.C#/AULA 04/Musica.cs
+++ !C#/C2.C#/AULA 04/Musica.cs
@@ -1,34 +1,32 @@
 class Musica
 {
-    //Um construtor é um método especial que é executado no momento da criação do objeto.
-    public Musica(Banda artista, string nomeMusica)
+    public Musica(Banda artista, string nome)
     {
-        ArtistaMusica = artista;
-        NomeMusica = nomeMusica;
+        Artista = artista;
+        Nome = nome;
     }
-    public string NomeMusica {  get; }
-    //o artista não é mais um texto (string), é a própria Banda, ou seja, a própria classe.
-    public Banda ArtistaMusica { get; }
-    public int DuracaoMusica { get; set; }
-    //letra de começo do atributo maiúscula pq é uma propriedade
-    public bool DisponivelMusica {  get; set; }
-    public string ResumoC =>
-        $"A música {NomeMusica} pertence ao artista {ArtistaMusica}";
 
-    public void FichaTecnica()
+    public string Nome { get; }
+    public Banda Artista { get; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => 
+        $"A música {Nome} pertence à banda {Artista}";
+
+
+
+
+    public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Música: {NomeMusica}");
-        Console.WriteLine($"Artista: {ArtistaMusica.Nome}");
-        Console.WriteLine($"Duração: {DuracaoMusica}");
-        if (DisponivelMusica)
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
         {
-            Console.WriteLine("Disponível no plano");
-        }
-        else
+            Console.WriteLine("Disponível no plano.");
+        } else
         {
-            Console.WriteLine("Indisponível no plano atual, adquira o plano Plus+");
+            Console.WriteLine("Adquira o plano Plus+");
         }
-        Console.WriteLine(" ");
     }
 }
-

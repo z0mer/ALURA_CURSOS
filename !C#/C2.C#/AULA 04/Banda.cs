15fed9837913d31c488fdac8ed7ca2c472544126
Anna Carolina Zomer <117862465+z0mer@users.noreply.github.com>
2024-09-11 14:50:45 -0300

    Update Banda.cs

--- !C#/C2.C#/AULA 04/Banda.cs
+++ !C#/C2.C#/AULA 04/Banda.cs
@@ -1,19 +1,22 @@
-class Banda
+﻿class Banda
 {
     private List<Album> albums = new List<Album>();
-    //Ao digitar ctor e apertar "Tab", o Visual Studo gera o modelo de um construtor.
+
     public Banda(string nome)
     {
         Nome = nome;
     }
+
     public string Nome { get; }
-    public void AdicionarAlbum(Album album)
-    {
+
+    public void AdicionarAlbum(Album album) 
+    { 
         albums.Add(album);
     }
+
     public void ExibirDiscografia()
     {
-        Console.WriteLine($"\nDiscografia da banda {Nome}\n");
+        Console.WriteLine($"Discografia da banda {Nome}");
         foreach (Album album in albums)
         {
             Console.WriteLine($"Álbum: {album.Nome} ({album.DuracaoTotal})");

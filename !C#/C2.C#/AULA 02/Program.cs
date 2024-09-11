0b0377a3ec35ae56fffa3a850e085d5c035441b0
Anna Carolina Zomer <117862465+z0mer@users.noreply.github.com>
2024-09-11 10:57:17 -0300

    Update Program.cs

--- !C#/C2.C#/AULA 02/Program.cs
+++ !C#/C2.C#/AULA 02/Program.cs
@@ -1,1 +1,16 @@
+Musica musica1 = new Musica();
+musica1.NomeMusica = "Another Love";
+musica1.ArtistaMusica = "Tom Odell";
+musica1.DuracaoMusica = 244;
+musica1.DisponivelMusica = true;
+Console.WriteLine(musica1.ResumoC);
 
+Musica musica2 = new Musica();
+musica2.NomeMusica = "Easy On Me";
+musica2.ArtistaMusica = "Adele";
+musica2.DuracaoMusica = 224;
+musica2.DisponivelMusica = false;
+Console.WriteLine(musica2.DisponivelMusica);
+
+musica1.FichaTecnica();
+musica2.FichaTecnica();

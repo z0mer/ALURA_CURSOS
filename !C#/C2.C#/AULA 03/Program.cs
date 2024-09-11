acf5f1609a85f190d8b40a1b652f9b676e0cc0f6
Anna Carolina Zomer <117862465+z0mer@users.noreply.github.com>
2024-09-11 13:38:15 -0300

    Update Program.cs

--- !C#/C2.C#/AULA 03/Program.cs
+++ !C#/C2.C#/AULA 03/Program.cs
@@ -3,9 +3,11 @@ albumQueen.Nome = "A night at the opera";
 
 Musica musica1 = new Musica();
 musica1.NomeMusica = "Love of my life";
+musica1.DuracaoMusica = 213;
 
 Musica musica2 = new Musica();
 musica2.NomeMusica = "Bohemian Rhapsody";
+musica2.DuracaoMusica = 354;
 
 albumQueen.AdicionarMusica(musica1);
 albumQueen.AdicionarMusica(musica2);
